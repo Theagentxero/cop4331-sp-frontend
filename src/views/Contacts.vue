b908b078ab8fcfb357e7a95573a1b0aa49921636
Joel Mora <joelemora@yahoo.com>
2020-01-22 19:52:02 -0500

    Contacts update stuff

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="">
     <b-navbar toggleable="lg" type="dark" variant="dark">
-      <!-- <a class="navbar-brand" href="#">
+      <!-- <a class="navbar-brand" href="https://www.google.com" target="_blank">
         <img src="../assets/anonymous_crab_orange.png" alt="Crabbie" class="navbar-icon">
       </a> -->
+      <b-button v-b-modal.modal-1>
+        <img src="../assets/anonymous_crab_orange.png" alt="Crabbie" class="navbar-icon">
+      </b-button>
+
+      <b-modal id="modal-1" title="DELETE CRAB">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collpase id="nav-collapse" is-nav>
