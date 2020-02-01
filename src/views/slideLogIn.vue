a89c7458023d3f4272054ebc50cfd283cce17fb3
joelbolt35 <joelemora@yahoo.com>
2020-02-01 03:20:14 -0500

    Signing In and Logging out working properly

--- src/views/slideLogIn.vue
+++ src/views/slideLogIn.vue
@@ -5,21 +5,21 @@
             <h1>Sign Up</h1>
 			<br />
         <div>
-        <input v-model="signUpForm.fName" placeholder="First Name" id="signUpfName" type='text' style='display: inline; width: 132px; margin-right: 0.25cm;' />    
-        <input v-model="signUpForm.lName" placeholder="Last Name" id="signUplName" type='text' style='display: inline; width: 132px; margin-left: 0.25cm;' />
+        <input v-model="signUpForm.form.first_name" placeholder="First Name" id="signUpfName" type='text' style='display: inline; width: 132px; margin-right: 0.25cm;' />    
+        <input v-model="signUpForm.form.last_name" placeholder="Last Name" id="signUplName" type='text' style='display: inline; width: 132px; margin-left: 0.25cm;' />
 		</div>
-		<input v-model="signInForm.email" id="signUpEmail" type="email" placeholder="Email">
-		<input v-model="signUpForm.password" id="signUpPassword" type="password" placeholder="Password">
-		<input v-model="signUpForm.verifyPassword" id="signUpotherPass" type="password" placeholder="Re-enter password">
-			<button v-on:click="signUp()" >Sign Up</button>
+		<input v-model="username" id="signUpEmail" type="email" placeholder="Email">
+		<input v-model="signUpForm.form.password" id="signUpPassword" type="password" placeholder="Password">
+		<input v-model="signUpForm.form.verifyPassword" id="signUpotherPass" type="password" placeholder="Re-enter password">
+			<button v-on:click="signUp()">Sign Up</button>
 		</form>
 	</div>
 	<div class="form-container sign-in-container">
 		<form action="#">
 			<h1>Sign in</h1>
 			<br />
-			<input v-model="signInForm.email" type="email" placeholder="Email" id="signInEmail" />
-			<input v-model="signInForm.password" type="password" placeholder="Password" id="signInPass" :state="passMin" />
+			<input v-model="username" type="email" placeholder="Email" id="signInEmail" />
+			<input v-model="signInForm.form.password" type="password" placeholder="Password" id="signInPass" />
 			<button v-on:click="signIn()" >Sign In</button>
 		</form>
 	</div>
@@ -41,42 +41,197 @@
 </template>
 
 <script>
-export default {
+const axios = require('axios');
+const uuidv4 = require('uuid/v4');
+const jwt = require('jsonwebtoken');
+const _ = require('underscore');
+var sha256 = require('js-sha256');
 
+const instance = axios.create({
+    timeout: 10000,
+    headers: {'Content-Type': 'application/json'},
+    withCredentials: true
+});
+
+export default {
+	name:'login',
     data() {
         return {
+			username: '',
             signInForm: {
-                email:'',
-                password:'',
+                form: {
+					password:''
+				},
+				invalidLogin: null,
+				showIssueText: true,
+				issueText: 'ISSUE',
+				formWaiting: false,
 			},
-			
 			signUpForm: {
-				fName:'',
-				lName:'',
-				password:'',
-				verifyPassword:'',
+				form: {
+					first_name:'',
+					last_name:'',
+					password:'',
+					verifyPassword:''
+				},
+				formWaiting: false,
+				formComplete: false,
+				formFailed: false,
+				passwordVal: {
+					hasNumber: false,
+					hasUpper: false,
+					hasSpecial: false,
+					isLong: false,
+				},
+				passStrengthPillVariant: "secondary",
+				passStrengthPillLabel: "Nothing Entered Yet"
 			}
         }
+	},
+	computed: {
+        usernameInvalidFeedback() {
+            return "Must Be A Valid Email";
+        },
     },
-
     methods: {
         signIn: function() {
-            alert(JSON.stringify(this.signInForm))
+			event.preventDefault();
+			this.formWaiting = true;
+			this.signInForm.form.username = this.username;
+            var values = _.clone(this.signInForm.form);
+            values.password = sha256(values.password);
+            instance.post('api/auth/login.json', values)
+            .then(async (response) => {
+                this.formComplete = true;
+                this.formWaiting = false;
+                // Fetch User Info From userinfo Cookie
+                var userInfo = null;
+                var seekCookie = "userinfo=";
+                var allRawCookies = document.cookie;
+                //console.log(allRawCookies);
+                var decodedCookies = decodeURIComponent(allRawCookies);
+                //console.log(decodedCookies);
+                // Split Cookies
+                var cookiesAry = decodedCookies.split(';');
+                // Find The Correct Cookie
+                var match = null;
+                cookiesAry.forEach(raw => {
+                    var loc = raw.indexOf(seekCookie);
+                    if(loc != -1){
+                        // Found The Cookie With Our Name
+                        match = raw;
+                    }
+                });
+                if(match != null){
+                    var tmpVal = match.substring(seekCookie.length, match.length);
+                    userInfo = JSON.parse(tmpVal)
+                    console.log(userInfo);
+                    this.$store.commit('login', userInfo);
+                    if(this.$store.getters.isAuthenticated){
+                        this.$router.push({path:"/home"});
+                    }else{
+                        console.log("Not Authenticated");
+                    }
+                }else{
+                    // TODO: Make Some User Alert To Inform User That Cookie Is Missing
+                }
+            })
+            .catch((error) => {
+                this.formFailed = true;
+                this.formWaiting = false;
+                // TODO: Handle Errors
+            });
+            console.log(this.signInForm)
         },
-
         signUp: function() {
-			var x = this.signUpForm;
-			x.email = this.signInForm.email;
-            alert(JSON.stringify(x));
-        },
+			event.preventDefault();
+            if(this.signUpForm.form.password != this.signUpForm.form.verifyPassword){
+                // TODO: Add Form Alert When Not Matching
+                return null;
+            }
+			this.formWaiting = true;
+			this.signUpForm.form.username = this.username;
+            var values = _.clone(this.signUpForm.form);
+            console.log(values)
+            values.password = sha256(values.password);
+            values.verifyPassword = null;
+            console.log(values)
+            axios.post('api/auth/create.json', values)
+            .then((response) => {
+                this.formComplete = true;
+                this.formWaiting = false;
+                console.log(response);
+            })
+            .catch((error) => {
+                this.formFailed = true;
+                this.formWaiting = false;
+                if(error.response.data.status == 403){
+                    // TODO: This means that the user already exits
+                    // Put Something On The Screen To Say That
+                }
+                console.log(error);
+                console.log(error.response.data.status);
+                console.log(error.response.data.info);
+            });
+            //alert(JSON.stringify(this.form));
+		},
+		checkPasswordRequirements(){
+            var isEightOrLonger = /^(?=.{8,72})/;
+            var hasUppercase = /^(?=.*[A-Z])/;
+            var hasNumber = /^(?=.*[0-9])/;
+            var specialCharacters = /[^A-Za-z0-9]+/;
+            var hasSpecialChar= /^(?=.[!@#\$%\^&])/;
 
+            this.passwordVal.isLong = isEightOrLonger.test(this.form.password);
+            this.passwordVal.hasNumber = hasNumber.test(this.form.password);
+            this.passwordVal.hasSpecial = specialCharacters.test(this.form.password);
+            this.passwordVal.hasUpper = hasUppercase.test(this.form.password);
+		},
+		passwordStrength(){
+            this.checkPasswordRequirements();
+            var str = 0; 
+            var meetsMin = false;
+            if(this.passwordVal.isLong){str = str + 1; meetsMin = true;}
+            if(this.passwordVal.hasNumber){str = str + 1;}
+            if(this.passwordVal.hasSpecial){str = str + 1;}
+            if(this.passwordVal.hasUpper){str = str + 1;}
+            if(!meetsMin){
+                str = 0;
+            }
+            console.log(str);
+            if(str == 0){
+                this.passStrengthPillLabel = "Unsuitable - Must Be 8 Characters Long"
+                this.passStrengthPillVariant = "danger"
+            }
+            if(str == 1){
+                this.passStrengthPillLabel = "Weak"
+                this.passStrengthPillVariant = "warning"
+            }
+            if(str == 2){
+                this.passStrengthPillLabel = "Marginal"
+                this.passStrengthPillVariant = "secondary"
+            }
+            if(str == 3){
+                this.passStrengthPillLabel = "Good"
+                this.passStrengthPillVariant = "info"
+            }
+            if(str == 4){
+                this.passStrengthPillLabel = "Excellent"
+                this.passStrengthPillVariant = "success"
+            }
+		},
+		usernameIsEmail(event){
+            var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            console.log("Blur Email");
+            this.state.username = emailRegex.test(this.form.username);
+		},
+		
         moveRight: function() {
             const container = document.getElementById('container');
             container.classList.remove('right-panel-active')
             // document.getElementById('signInEmail').value = "";
             document.getElementById('signInPass').value = "";
         },
-
         moveLeft: function() {
             const container = document.getElementById('container');
 			container.classList.add('right-panel-active')
@@ -84,6 +239,13 @@ export default {
 			document.getElementById('signUplName').value = "";
 			document.getElementById('signUpPassword').value = "";
 			document.getElementById('signUpotherPass').value = "";
+		},
+		onChange(value) {
+			this.signUpForm.email = this.signInForm.username;
+            if(this.showIssueText){
+                this.showIssueText = false;
+                this.invalidLogin = null;
+            }
         }
     }
 }
