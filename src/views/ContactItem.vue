ba29b6b413307a58cc367945683a0a77458dec58
joelbolt35 <joelemora@yahoo.com>
2020-02-08 18:31:35 -0500

    nothing to see here

--- src/views/ContactItem.vue
+++ src/views/ContactItem.vue
@@ -1,15 +1,16 @@
-<template lang="pug">
-b-col.text-center(v-if='!selected', cols='6', sm='4', md='3', lg='2')
-  b-button(variant='our-orange', :pressed.sync='selected')
-    img.contact-icon.mt-2(:src="'/img/contact/' + contact._id")
-  h1(v-if="contact.firstName != ''") {{contact.firstName}}
-  h1(v-else-if="contact.middleName != ''") {{contact.middleName}}
-  h1(v-else-if="contact.lastName != ''") {{contact.lastName}}
-  h1(v-else-if='contact.phoneNumbers.length != 0') {{contact.phoneNumbers[0].value}}
-  h1(v-else-if='contact.emails.length != 0') {{contact.emails[0].value}}
-  h1(v-else='') No Info
-b-col(v-else='', cols='12')
-  EditContact(@closeComponent='updateSelected', :initContact='contact')
+<template>
+<b-col v-if="!selected" cols="6" sm="4" md="3" lg="2" class="text-center">
+  <b-button variant="our-orange" :pressed.sync="selected"><img :src="'/img/contact/' + contact._id" class="contact-icon mt-2"/></b-button>
+  <h1 v-if="contact.firstName != ''">{{contact.firstName}}</h1>
+  <h1 v-else-if="contact.middleName != ''">{{contact.middleName}}</h1>
+  <h1 v-else-if="contact.lastName != ''">{{contact.lastName}}</h1>
+  <h1 v-else-if="contact.phoneNumbers.length != 0">{{contact.phoneNumbers[0].value}}</h1>
+  <h1 v-else-if="contact.emails.length != 0">{{contact.emails[0].value}}</h1>
+  <h1 v-else="">No Info</h1>
+</b-col>
+<b-col v-else="" cols="12">
+  <EditContact @closeComponent="updateSelected" :initContact="contact"></EditContact>
+</b-col>
 </template>
 
 <script>
