9f45c6c49009c011620815d401c0bf170dae29c1
joelbolt35 <joelemora@yahoo.com>
2020-02-14 19:34:22 -0500

    css changes by victor

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,9 @@
 <style lang="scss">
 #app {
   height: 100vh;
-  background: linear-gradient(70deg, #aec6cf 60%, #c3b091 60%);
+  background: linear-gradient(110deg, #aec6cf 50%, #c3b091 50%);
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 </style>
-
-// a0e7e5
